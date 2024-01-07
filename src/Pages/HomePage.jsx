9c9594c097913511044a638e6b3a505be15678ec
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-07 21:15:44 +0530

    Updated the HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Container , Flex ,Box} from '@chakra-ui/react'
+import FeedPosts from '../components/FeedPosts/FeedPosts'
 
 function HomePage() {
   return (
     <>
-        <div>HomePage</div>
+        <Container maxWidth={'Container.lg'}>
+          <Flex gap={20}>
+            <Box flex={2} py={20}
+            // border={'1px solid blue'}
+            >
+              <FeedPosts />
+            </Box>
+            <Box flex={3} mr={20}
+            display={{base:"none",lg:"block"}}
+            maxWidth={'300px'}
+            // border={'1px solid red'}
+            >
+              Suggested
+            </Box>
+          </Flex>           
+        </Container>
     </>
   )
 }
