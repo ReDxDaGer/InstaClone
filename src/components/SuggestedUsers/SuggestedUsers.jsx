18f82176f7d7fbc2eae9030e92db647f927552cb
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-14 20:19:20 +0530

    Created the file of Suggested Users section

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Flex, Text, VStack } from "@chakra-ui/react"
+import SuggestedHeader from "./SuggestedHeader"
 
-function SuggestedUsers() {
+const SuggestedUsers = () => {
   return (
-    <div>SuggestedUsers</div>
+    <>
+    <VStack 
+    py={8} 
+    px={6} 
+    gap={4}
+    >
+      <SuggestedHeader />
+
+      <Flex alignItems={'center'}
+      justifyContent={'space-between'}
+      w={'full'}
+      >
+        <Text fontSize={12} fontWeight={"bold"} color={'gray.500'}>
+          Suggested For you
+        </Text>
+        <Text fontSize={12} fontWeight={"bold"} color={'blue.500'} cursor={'pointer'}
+        _hover={{
+          textDecoration:"underline"
+        }}
+        >
+          See all
+        </Text>
+      </Flex>
+    </VStack>
+    </>
   )
 }
 
